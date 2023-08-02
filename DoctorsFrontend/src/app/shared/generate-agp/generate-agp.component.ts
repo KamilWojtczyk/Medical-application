ec6c643e6419840f78e69b925f7d421d670597f0
Kamil Wojtczyk <wojtczykkamil@gmail.com>
2023-08-02 21:28:19 +0200

    agp chart update

--- DoctorsFrontend/src/app/shared/generate-agp/generate-agp.component.ts
+++ DoctorsFrontend/src/app/shared/generate-agp/generate-agp.component.ts
@@ -7,12 +7,15 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-agp-chart',
   templateUrl: './generate-agp.component.html',
+  styleUrls: ['./generate-agp.component.scss'],
 })
 export class AgpChartComponent implements OnInit {
   moment: any = moment;
 
-  @Input() patientMinGlucose = 70;
-  @Input() patientMaxGlucose = 115;
+  @Input() patientMinGlucoseEmptyStomach = 70;
+  @Input() patientMaxGlucoseEmptyStomach = 99;
+  @Input() patientMaxGlucoseAfterMeal = 140;
+  @Input() patientWithDiabetes = 180;
 
   hours = Array.from({ length: 24 }, (_, i) => `${i}:00`);
 
@@ -116,18 +119,38 @@ export class AgpChartComponent implements OnInit {
             borderDash: [5, 5],
           },
           {
-            label: 'Patient Max Glucose',
-            data: Array(24).fill(this.patientMaxGlucose),
+            label: 'Patient with probable diabetes',
+            data: Array(24).fill(this.patientWithDiabetes),
+            borderColor: 'red',
+            borderWidth: 2,
+            fill: {
+              target: 'end',
+            },
+            backgroundColor: 'rgba(255, 69, 0, 0.1)',
+          },
+          {
+            label: 'Patient Max Accepted Glucose After Meal',
+            data: Array(24).fill(this.patientMaxGlucoseAfterMeal),
+            borderColor: 'orange',
+            borderWidth: 2,
+            fill: {
+              target: 5,
+            },
+            backgroundColor: 'rgba(255, 165, 0, 0.2)',
+          },
+          {
+            label: 'Patient Max Glucose On Empty Stomach',
+            data: Array(24).fill(this.patientMaxGlucoseEmptyStomach),
             borderColor: 'green',
             borderWidth: 2,
             fill: {
-              target: 4,
-              above: 'rgba(11, 156, 49, 0.2)',
+              target: 6,
             },
+            backgroundColor: 'rgba(11, 156, 49, 0.2)',
           },
           {
-            label: 'Patient Min Glucose',
-            data: Array(24).fill(this.patientMinGlucose),
+            label: 'Patient Min Glucose On Empty Stomach',
+            data: Array(24).fill(this.patientMinGlucoseEmptyStomach),
             borderColor: 'green',
             borderWidth: 2,
             fill: false,
